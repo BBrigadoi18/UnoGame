c00857d079b3bfa7dd10f7c254dab7dad3f29090
BBrigadoi18 <128743596+BBrigadoi18@users.noreply.github.com>
2023-04-01 09:40:24 -0400

    Add files via upload

---
+++ Uno Game.py
@@ -0,0 +1,14 @@
+# Bridget Brigadoi and Abby Thompson
+# April 1, 2023
+
+# Similulating an Uno Game 
+
+uno_cards_dict = {1: ("0", "Red"), 2: ("1", "Red"), 3: ("2", "Red"), 4: ("3", "Red"), 5: ("4", "Red"), 6: ("5", "Red"), 7: ("6", "Red"), 8: ("7", "Red"), 9: ("8", "Red"), 
+                      10: ("9", "Red"), 11: ("1", "Red"), 12: ("2", "Red"), 13: ("3", "Red"), 14: ("4", "Red"), 15: ("5", "Red"),  16: ("6", "Red"), 17: ("7", "Red"), 18: ("8", "Red"), 
+                      19: ("9", "Red"), 
+                      39: ("0", "Green"), 40: ("1", "Green"), 41: ("2", "Green"), 42: ("3", "Green"), 43: ("4", "Green"), 44: ("5", "Green"), 45: ("6", "Green"), 46: ("7", "Green"),
+                      47: ("8", "Green"), 48: ("9", "Green"), 49: ("1", "Green"), 50: ("2", "Green"), 51: ("3", "Green"), 52: ("4", "Green"), 53: ("5", "Green"), 54: ("6", "Green"), 
+                      55: ("7", "Green"), 56: ("8", "Green"), 57: ("9", "Green"),
+                      77: ("Draw 2", "Red"), 78: ("Draw 2", "Red"), 79: ("Draw 2", "Blue"), 80: ("Draw 2", "Blue"), 81: ("Draw 2", "Green"), 82: ("Draw 2", "Green"),  83: ("Draw 2", "Yellow"),
+                      84: ("Draw 2", "Yellow"), 85: ("Wild", "Black"), 86: ("Wild", "Black"), 87: ("Wild", "Black"), 88: ("Wild", "Black"), 89: ("Draw 4", "Black"), 85: ("Draw 4", "Black"),
+                      85: ("Draw 4", "Black"), 85: ("Draw 4", "Black"),}
