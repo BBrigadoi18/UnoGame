91c6af48a5464ce3b1d946dc029837b967ee60ba
abbythompson2 <129601900+abbythompson2@users.noreply.github.com>
2023-04-01 09:42:01 -0400

    Add files via upload

---
+++ unogame1.py
@@ -0,0 +1,18 @@
+#Abby Thompson, Bridget Brigadoi
+
+
+uno_card_dict= {20: ("0", "Blue"), 21: ("1", "Blue"), 22: ("2", "Blue"), 23: ("3", "Blue"), 24: ("4", "Blue"), 25: ("5", "Blue"),
+                26: ("6", "Blue"), 27: ("7", "Blue"), 28: ("8", "Blue"), 29: ("9", "Blue"), 30: ("1", "Blue"), 31: ("2", "Blue"),
+                32: ("3", "Blue"), 33: ("4", "Blue"), 34: ("5", "Blue"), 35: ("6", "Blue"), 36:("7", "Blue"), 37: ("8", "Blue"), 
+                38:("9", "Blue"), 
+                
+                58: ("0", "Yellow"), 59: ("1", "Yellow"), 60: ("2", "Yellow"), 61: ("3", "Yellow"), 62: ("4", "Yellow"), 63: ("5", "Yellow"), 
+                64: ("6", "Yellow"), 65: ("7", "Yellow"), 66: ("8", "Yellow"), 67: ("9", "Yellow"), 68:("1", "Yellow"), 69: ("2", "Yellow"),  
+                70: ("3", "Yellow"), 71: ("4", "Yellow"), 72: ("5", "Yellow"), 73: ("6", "Yellow"), 74: ("7", "Yellow"), 75: ("8", "Yellow"), 
+                76: ("9", "Yellow"),
+
+
+                93: ("skip", "Yellow"), 94: ("skip", "Yellow"), 95: ("skip", "Red"), 96: ("skip", "Red"), 97: ("skip", "Green"), 98: ("skip", "Green"), 
+                99: ("skip", "Blue"), 100: ("skip", "Blue")}
+                
+                 
